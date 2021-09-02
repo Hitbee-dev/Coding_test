1df44b781cf2ab28f63468e9f4fd9893497049e0
Hitbee-dev <ckim.esw@gmail.com>
2021-09-02 14:56:13 +0900

    ex1

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+array = [3, 5, 1, 2, 4]
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+for i in array:
+    for j in array:
+        temp = i*j
+        print(temp)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# [1, 2, 3, 4, 5], [2, 4, 6, 8, 10].. 이렇게 나올줄 알았는데
+# index값을 곱해주는거군... 기초부터 시작...
