4b3a2a5a2fb6a484940ba27cf3c12a2d3f501d3b
Hitbee-dev <ckim.esw@gmail.com>
2021-09-04 03:04:46 +0900

    큰 수의 법칙 정답

--- contents/big_num.py
+++ contents/big_num.py
@@ -20,17 +20,12 @@ data.sort() #입력받은 수 정렬
 first = data[n-1] 
 second = data[n-2] #두번째로 큰 수
 
-result = 0
+#가장 큰 수가 더해지는 횟수 계산
+count = int(m / (k+1)) * k
+count += m % (k+1)
 
-while(True):
-    for i in range(k): #갸쟝 큰 수를 K번 더하기
-        if(m == 0): #만약 m이 0이라면(더해지는 횟수가 0이라면) 반복문 탈출
-            break
-        result += first
-        m -= 1 #더할 때 마다 더할 횟수 빼주기
-    if(m == 0): #만약 m이 0이라면(더해지는 횟수가 0이라면) 반복문 탈출
-        break
-    result += second #두번째로 큰 수를 한 번 더하기
-    m -= 1 #더할 때 마다 1씩 빼기
+result = 0
+result += (count) * first #가장 큰 수 더하기
+result += (m-count) * second #두번째로 큰 수 더하기
 
 print(result)
