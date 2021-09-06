2791e199e7fdce84190dbc9c492346c1bab57ce8
Hitbee-dev <ckim.esw@gmail.com>
2021-09-06 18:44:28 +0900

    programmus folder remove

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-# from programmus import example1
-from study import quiz1
+from contents import big_num
+# from study import quiz1
--- study/quiz1.py
+++ study/quiz1.py
@@ -1,21 +1,25 @@
 #어느 달력에서 토요일 날짜를 모두 더했더니 80이었다.
-#이달 13일은 무슨 요일일까?
+#이달 k일은 무슨 요일일까?
 
-#토요일이 2일부터 시작되어야 함
-#토요일이 5번 나와야 한다.
+#입력 조건
 #예외처리를 해야한다.
 #이달 k일은 무슨요일인지 출력해야한다.
+
+#출력 조건
+#토요일 시작일: days
+#k일: k
+#월요일
 
 weekdays = ["목", "금", "토", "일", "월", "화", "수"] #토요일이 2일이니, 0=목, 1=금, 2=토...Index지정
 
 result = 0
 
-days = int(input("토요일은 몇일부터 시작되야 할까?: "))
+days = int(input("토요일 시작일: "))
 if not(days == 2):
     print("정답이 아닙니다.")
 
 if(days==2):
-    k = int(input("k일은 무슨 요일인가?: "))
+    k = int(input("k일: "))
     print(f"days: 1주차 {days}일") #입력한 날짜
     result += days
     for i in range(4): #토요일은 5번 나와야 한다. 하지만 2일은 맨처음에 입력 해 주었으니, 4번만 더 반복
@@ -26,4 +30,4 @@ if(days==2):
         print(f"days: {i+2}주차 {days}일") #날짜
         print(f"result: {result}") #토요일을 모두 더했을 때의 값
 
-    print(f"요일: {weekdays[k]}")
+    print(f"{weekdays[k]}요일")
