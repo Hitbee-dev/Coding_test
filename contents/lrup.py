fc6ffede087d9144005febd8a2b181038afc20dc
Hitbee-dev <ckim.esw@gmail.com>
2021-09-07 16:30:16 +0900

    c언어 확장

--- contents/lrup.py
+++ contents/lrup.py
@@ -68,10 +68,9 @@ move_types = ["L", "R", "U", "D"]
 
 #이동 계획을 하나씩 확인
 for plan in plans:
-    print(f"plans: {plans}")
     #이동 후 좌표 구하기
-    for i in range(len(move_types)):
-        if(plan == move_types[i]):
+    for i in range(len(move_types)): #입력할 수 있는 커맨드 갯수만큼 반복
+        if(plan == move_types[i]): #만약 해당되는 커맨드가 있다면
             nx = x+dx[i]
             ny = y+dy[i]
     #공간을 벗어나는 경우 무시
@@ -80,5 +79,3 @@ for plan in plans:
     #이동 수행
     x, y = nx, ny
 print(x, y)
-        
-        
