1983c702941b359e112de11ebe8ec6e28a37d3ea
Hitbee-dev <ckim.esw@gmail.com>
2021-09-11 21:45:58 +0900

    programmus 완주하지 못한 선수 문제 풀이

---
+++ programmus/solution1.py
@@ -0,0 +1,43 @@
+# 문제 설명
+#수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+#
+#마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+#
+#제한사항
+#마라톤 경기에 참여한 선수의 수는 1명 이상 100, 000명 이하입니다.
+#completion의 길이는 participant의 길이보다 1 작습니다.
+#참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
+#참가자 중에는 동명이인이 있을 수 있습니다.
+#입출력 예
+#participant	completion return
+#["leo", "kiki", "eden"]["eden", "kiki"]	"leo"
+#["marina", "josipa", "nikola", "vinko", "filipa"]["josipa", "filipa", "marina", "nikola"]	"vinko"
+#["mislav", "stanko", "mislav", "ana"]["stanko", "ana", "mislav"]	"mislav"
+#입출력 예 설명
+#예제  # 1
+#"leo"는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.
+#
+#예제  # 2
+#"vinko"는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.
+#
+#예제  # 3
+#"mislav"는 참여자 명단에는 두 명이 있지만, 완주자 명단에는 한 명밖에 없기 때문에 한명은 완주하지 못했습니다.
+
+participant = ["mislav", "stanko", "mislav", "ana"]
+completion = ["stanko", "ana", "mislav"]
+
+def solution(participant, completion):
+    answer = ''
+    participant.sort()  # 정렬
+    completion.sort()  # 정렬
+    for n in range(len(completion)):
+        if(participant[n] != completion[n]):  # 정렬 후 참여선수와 완주 선수를 비교, 만약 원소값이 같지 않다면
+            answer = participant[n]  # 이 참여 선수는 완주하지 못한 것이므로 출력
+            return answer
+
+    if(answer == ''):  # 전부 다 돌렸는데, 안나왔다면
+        # 이름이 제일 마지막 순서이기 때문에, 마지막 선수 이름 출력
+        answer = participant[len(completion)]
+        return answer
+
+print(solution(participant, completion))
