a0a7680ff31ee028a0f0f39ae6e75916e687d036
Hitbee-dev <ckim.esw@gmail.com>
2021-09-16 05:00:33 +0900

    과제 문제 추가

---
+++ algorithm/homework1.py
@@ -0,0 +1,8 @@
+#기본 정렬 알고리즘을 직접 구현하고, 입력에 따른 수행시간을 축정해서 차이를 비교해본다.
+
+# 1) 입력 데이터 생성(2점)
+#   - Generate input data
+# 2) 버블 정렬, 선택 정렬, 삽입 정렬 알고리즘 구현(1점, 1점, 3점)
+#   - Implement 3 basic sorting algorithms
+# 3) 각 입력에 따른 수행 시간 측정(3점)
+#   - Input generated data and measure execution times
