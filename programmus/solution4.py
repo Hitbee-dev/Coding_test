9239dfc0c74206bf1fdbb3b1ca355b4738c213e8
Hitbee-dev <ckim.esw@gmail.com>
2021-09-17 17:07:33 +0900

    weekly 1week 풀이

--- programmus/solution4.py
+++ programmus/solution4.py
@@ -13,3 +13,14 @@
 #입출력 예 설명
 #입출력 예  # 1
 #이용금액이 3인 놀이기구를 4번 타고 싶은 고객이 현재 가진 금액이 20이라면, 총 필요한 놀이기구의 이용 금액은 30 (=3+6+9+12) 이 되어 10만큼 부족하므로 10을 return 합니다
+
+def solution(price, money, count):
+    fuc = 0
+
+    for c in range(count):
+        fuc += price*(c+1)
+
+    if(money-fuc < 0):
+        return abs(money-fuc)
+    else:
+        return 0
