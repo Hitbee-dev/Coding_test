4766d574af4caa17e6ddf02b7fe5ac6ed7b81e8c
Hitbee-dev <ckim.esw@gmail.com>
2021-09-25 22:53:49 +0900

    example2 clear

--- algorithm/hw1.py
+++ algorithm/hw1.py
@@ -1,12 +1,3 @@
-#입력 데이터 생성
-#- 양의 정수 N, M을 입력 받는다.
-#- 1) N은 각 입력의 데이터 개수 No. of integers in each test case input
-#     2 <= N <= 10,000,000
-#- 2) M은 생성할 테스트 케이스의 개수 No. of test case inputs to be generated
-#     3 <= M <= nPn <= 1,000
-#- M개의 테스트 입력을 생성한다. generate M test case inputs
-#- 1) 각 테스트 케이스에는 1, 2,... ,N이 저장된다.
-#     Each test case includes N integers: 1, 2,... ,(N-1)
 import random
 
 input_n, input_m = map(int, input("N M = ").split()) #N M = {} {} 입력받기
@@ -27,6 +18,41 @@ def convert_string(convert_integer): #출력될 문자열
     buffer_string = [str(integer) for integer in convert_integer]
     return " ".join(buffer_string)
 
+def bubble_sort(data): #버블 정렬
+    print("") # 공백
+    print("[T2] <bubble sort>")
+    print(f"L0: {convert_string(data)}")
+    for i in range(len(data)-1):
+        for j in range(len(data)-1-i):
+            if(data[j] > data[j+1]):
+                data[j], data[j+1] = data[j+1], data[j]
+        print(f"L{i+1}: {convert_string(data)}")
+
+def selection_sort(data):
+    print("")  # 공백
+    print("[T2] <selection sort>")
+    print(f"L0: {convert_string(data)}")
+    for i in range(0, len(data)-1):
+        buffer = i
+        for j in range(i+1, len(data)):
+            if(data[buffer] > data[j]):
+                buffer = j
+        data[i], data[buffer] = data[buffer], data[i]
+        print(f"L{i+1}: {convert_string(data)}")
+
+def insertion_sort(data):
+    print("")  # 공백
+    print("[T2] <insertion sort>")
+    print(f"L0: {convert_string(data)}")
+    for i in range(1, len(data)):
+        buffer = data[i]
+        j = i-1
+        while j >= 0 and data[j] > buffer:
+            data[j+1] = data[j]
+            j = j-1
+        data[j+1] = buffer
+        print(f"L{i+1}: {convert_string(data)}")
+
 make_datas = make_data(input_n, input_m)
 if(input_n <= 10 and input_m <= 10):
     for i in range(input_m):
@@ -41,3 +67,7 @@ if(input_n <= 10 and input_m <= 10):
 else:
     print(f"{input_m} test case inputs generated.")
     print(f"{input_n} integers in each test case.")
+
+bubble_sort(make_datas[2][:])
+selection_sort(make_datas[2][:])
+insertion_sort(make_datas[2][:])
