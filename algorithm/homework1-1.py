dd886056a19881f31d9288ba723e28b19ac4cf43
Hitbee-dev <ckim.esw@gmail.com>
2021-09-25 16:48:30 +0900

    algorithm homework1 문제

---
+++ algorithm/homework1-1.py
@@ -0,0 +1,9 @@
+#입력 데이터 생성
+#- 양의 정수 N, M을 입력 받는다.
+#- 1) N은 각 입력의 데이터 개수 No. of integers in each test case input
+#     2 <= N <= 10,000,000
+#- 2) M은 생성할 테스트 케이스의 개수 No. of test case inputs to be generated
+#     3 <= M <= 1,000
+#- M개의 테스트 입력을 생성한다. generate M test case inputs
+#- 1) 각 테스트 케이스에는 1, 2,... ,N이 저장된다.
+#     Each test case includes N integers: 1, 2,... ,(N-1)
