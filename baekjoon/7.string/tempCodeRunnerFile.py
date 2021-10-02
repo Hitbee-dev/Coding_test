61a02add61b732e44e86fadcd374c4f03c36b69c
Hitbee-dev <ckim.esw@gmail.com>
2021-10-03 02:48:32 +0900

    5622

---
+++ baekjoon/7.string/tempCodeRunnerFile.py
@@ -0,0 +1,21 @@
+:
+#     print(n[i])
+#     if(n[i] == "A" or "B" or "C"):
+#         result += 2
+#     elif(n[i] == "D" or "E" or "F"):
+#         result += 3
+#     elif(n[i] == "G" or "H" or "I"):
+#         result += 4
+#     elif(n[i] == "J" or "K" or "L"):
+#         result += 5
+#     elif(n[i] == "M" or "N" or "O"):
+#         result += 6
+#     elif(n[i] == "P" or "Q" or "R" or "S"):
+#         result += 7
+#     elif(n[i] == "T" or "U" or "V"):
+#         result += 8
+#     elif(n[i] == "W" or "X" or "Y" or "Z"):
+#         result += 9
+#     print(result)
+
+# print(result)
