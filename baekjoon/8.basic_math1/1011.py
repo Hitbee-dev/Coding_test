5d7b4396587fc73667eabadcb69e035a8c70c9c4
Hitbee-dev <ckim.esw@gmail.com>
2021-10-09 03:25:17 +0900

    basic_math1 clear

--- baekjoon/8.basic_math1/1011.py
+++ baekjoon/8.basic_math1/1011.py
@@ -9,7 +9,7 @@ for i in range(t):
     move2 = 0
     cnt = 0
 
-    while n <= m:
+    while n <= m: #시작 값과 목표값을 동시에 줄이기 시작
         if(move1 != max_cnt):
             move1 += 1
         cnt += 1
@@ -17,7 +17,7 @@ for i in range(t):
         m = m-move1
         # print(f"cnt: {cnt}, front_move: {move1}, n: {n}, m: {m}")
 
-    while m > init_n:
+    while m > init_n: #시작 값보다 작아질 때 까지 반복
         if(m-move2 != 0):
             move2 += 1
         cnt += 1
