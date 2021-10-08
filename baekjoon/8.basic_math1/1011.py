ceaeb27aa7aa92a68dc7edee2f192b50a47d5ed3
Hitbee-dev <ckim.esw@gmail.com>
2021-10-09 03:23:54 +0900

    1011

---
+++ baekjoon/8.basic_math1/1011.py
@@ -0,0 +1,27 @@
+import math
+
+t = int(input())
+for i in range(t):
+    n, m = map(int, input().split())
+    init_n = n
+    max_cnt = int(math.sqrt(m-n))
+    move1 = 0
+    move2 = 0
+    cnt = 0
+
+    while n <= m:
+        if(move1 != max_cnt):
+            move1 += 1
+        cnt += 1
+        n = n+move1
+        m = m-move1
+        # print(f"cnt: {cnt}, front_move: {move1}, n: {n}, m: {m}")
+        # print(cnt)
+
+    while m > init_n:
+        if(m-move2 != 0):
+            move2 += 1
+        cnt += 1
+        m = m-move2
+        # print(f"cnt: {cnt}, back_move: {move2}, n: {n}, m: {m}")
+    print(cnt)
