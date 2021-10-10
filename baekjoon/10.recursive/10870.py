05a22a84b0e66cfd46a7193859792d9843dcb7ec
Hitbee-dev <ckim.esw@gmail.com>
2021-10-10 14:32:11 +0900

    10870

---
+++ baekjoon/10.recursive/10870.py
@@ -0,0 +1,12 @@
+n = int(input())
+buffer = [0, 1]
+for i in range(2, n+1):
+    buffer.append((buffer[i-2])+(buffer[i-1]))
+    # print(buffer)
+
+if(n == 0):
+    print(0)
+elif(n == 1):
+    print(1)
+else:
+    print(buffer[-1]) 
