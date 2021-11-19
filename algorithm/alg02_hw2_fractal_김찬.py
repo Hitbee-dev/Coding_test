da765eced573bad1fe2ab2f94c3283f81705eed5
Hitbee-dev <ckim.esw@gmail.com>
2021-11-19 18:39:22 +0900

    주석 modify

--- algorithm/alg02_hw2_fractal_김찬.py
+++ algorithm/alg02_hw2_fractal_김찬.py
@@ -14,9 +14,9 @@ def fractal_tree(length, angle):
 		t.forward(length) # 정해진 길이만큼 그림
 
 		t.right(angle) # 오른쪽으로 꺾을 각도
-		fractal_tree(length * r.uniform(0.6, 0.9), r.randint(10, 40)) # 재귀 호출로 다음 가지 이어서 그리도록(길이, 각도)
+		fractal_tree(length * r.uniform(0.6, 0.9), r.randint(10, 40)) # 재귀 호출로 다음 가지 이어서 그리도록(길이, 각도 랜덤)
 		t.left(angle * 2) # 왼쪽으로는 오른쪽각도의 2배만큼 꺾어준다.
-		fractal_tree(length * r.uniform(0.8, 0.9), r.randint(10, 40)) # 재귀 호출로 다음 가지 이어서 그리도록(길이, 각도)
+		fractal_tree(length * r.uniform(0.8, 0.9), r.randint(10, 40)) # 재귀 호출로 다음 가지 이어서 그리도록(길이, 각도 랜덤)
 		t.right(angle)
 		
 		t.pensize(length * 0.1)
