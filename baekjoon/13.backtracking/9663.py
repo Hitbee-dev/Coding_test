cc2f8c4a87d8bdf4bb9a424a24394952c076a657
Hitbee-dev <ckim.esw@gmail.com>
2021-11-29 15:53:27 +0900

    9663

--- baekjoon/13.backtracking/9663.py
+++ baekjoon/13.backtracking/9663.py
@@ -1,6 +1,6 @@
 import sys
 
-#내 윗줄에 나와 겹치는 라인에 퀸이 있는가?
+#현재위치 윗줄에 퀸이 겹치는지 확인
 def adjacent(x):
     for i in range(x):
         if row[x] == row[i] or abs(row[x] - row[i]) == x - i:
@@ -11,10 +11,8 @@ def adjacent(x):
 #한줄씩 재귀하며 DFS를 실행
 def dfs(x):
     global result
-    
     if x == N:
         result += 1
-
     else:
         for i in range(N):
             row[x] = i
