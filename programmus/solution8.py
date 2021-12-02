179b16cdeb8375519a2f9576603a894e0f95e764
Hitbee-dev <ckim.esw@gmail.com>
2021-12-03 01:56:30 +0900

    [해시]위상

--- programmus/solution8.py
+++ programmus/solution8.py
@@ -51,23 +51,46 @@ clothes = [
 # 각 옷의 종류당 +1을 하고, 옷의 종류를 모두 곱한 후 마지막에 1을 빼준다.
 # 이유는 옷을 고르지 않을 경우의 수를 더하기위해 +1을 해주고,
 # 1을 빼주는 이유는 모든 옷을 고르지 않을 때 이기 때문이다.
+# def multiply(arr):
+#     return eval('*'.join(map(str, arr)))
+
+# def solution(clothes):
+#     dic = {}
+#     arr = []
+#     for list in clothes:
+#         if list[1] not in dic:
+#             dic[list[1]] = 1
+#         else:
+#             dic[list[1]] += 1
+
+#     for i in dic.values(): # 옷의 종류 당 +1 씩 추가(안입는 경우)
+#         arr.append(i+1)
+    
+#     return multiply(arr) - 1 # 모두 다 안입는 경우를 빼주기위해 -1
+#     # print(multiply(arr) - 1)
+
+# 3번째 풀이방법 (정답) (코드 간결화)
 def multiply(arr):
     return eval('*'.join(map(str, arr)))
 
 def solution(clothes):
-    dic = {}
-    arr = []
-    for list in clothes:
-        if list[1] not in dic:
-            dic[list[1]] = 1
-        else:
-            dic[list[1]] += 1
+    arr = {}
+    for _, v in clothes:
+        if v not in arr:
+            arr[v] = 2
+        elif v in arr:
+            arr[v] += 1
+    return multiply(arr.values()) - 1
+    # print(multiply(arr.values()) - 1)
 
-    for i in dic.values(): # 옷의 종류 당 +1 씩 추가(안입는 경우)
-        arr.append(i+1)
+# 경수 풀이방법
+# from collections import Counter
+# from functools import reduce
+# def solution(clothes):
+#     counter = Counter(list(map(lambda x: x[1], clothes)))
+#     return reduce(lambda x, y: x * y, [_ + 1 for _ in counter.values()]) - 1
+#     # print(reduce(lambda x, y: x * y, [_ + 1 for _ in counter.values()]) - 1)
     
-    return multiply(arr) - 1 # 모두 다 안입는 경우를 빼주기위해 -1
-    # print(multiply(arr) - 1)
 
 for i in clothes:
     solution(i)
