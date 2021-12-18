88c20bc85a7ce3998af22ffa532825b4ab025863
Hitbee-dev <ckim.esw@gmail.com>
2021-12-18 18:53:52 +0900

    quicksort_inplace modify

--- algorithm/final_study/6. 정렬 알고리즘 III/quicksort_inplace.py
+++ algorithm/final_study/6. 정렬 알고리즘 III/quicksort_inplace.py
@@ -9,8 +9,10 @@ def quick_sort(arr, start, end):
     while left <= right:  # 작은값과 큰값을 비교하며 작은값을 찾음
         while left <= end and arr[left] <= arr[pivot]:
             left += 1  # 앞의 값이 마지막으로 비교할 때가 아니고, 앞의 값이 pivot 값보다 작을 때 오른쪽으로 한칸 이동
+            print(f"left: {left}")
         while right > start and arr[right] >= arr[pivot]:
             right -= 1  # 뒤의 값이 마지막으로 비교할 때가 아니고, 뒤의 값이 pivot 값보다 작을 때 왼쪽으로 한칸 이동
+            print(f"right: {right}")
         if left > right:  # 비교할 배열이 크로스가 되면
             temp = arr[right]  # 배열의 마지막 원소를 temp에 저장
             arr[right] = arr[pivot]  # 배열의 마지막 원소를 pivot으로 저장
@@ -24,5 +26,5 @@ def quick_sort(arr, start, end):
     quick_sort(arr, right+1, end)  # 큰값을 정렬
     return arr # 정렬된 배열을 반환
 
-data = [48, 1, 10, 5, 8, 13, 17, 20, 7, 6, 4, 3, 2, 9, 11,11, 12]
+data = [48, 1, 10, 5, 8, 13, 17, 20, 7, 6, 4, 3, 2, 9, 11, 11, 12]
 print(quick_sort(data, 0, len(data)-1)) 
