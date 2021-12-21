db678962bd725e91a5ed594d42c0ef637cb42585
Hitbee-dev <ckim.esw@gmail.com>
2021-12-21 13:09:08 +0900

    red_black_tree

--- algorithm/final_study/10. 검색 트리/red_black_tree.py
+++ algorithm/final_study/10. 검색 트리/red_black_tree.py
@@ -3,7 +3,7 @@
 # 2. root Node는 black이다.
 # 3. Leaf(None) Node는 black이다.
 # 4. red Node의 자식(Child)Node는 무조건 black이다.
-# 5. 각 Node에서 Leaf Node로 가는 black Node의 개수가 항상 같아야 한다.
+# 5. root Node에서 Leaf Node로 가는 black Node의 개수가 항상 같아야 한다.
 
 # h(v) = v의 높이(height)
 # bh(v) = v가 Leaf Node까지 갈 경우의 높이(height)
@@ -13,5 +13,3 @@
 # 사실1: v의 서브트리의 내부 Node개수 >= 2^bh(v) - 1
 # 증명: h(v)에 대한 귀납법
 # BestCase: h(v) = 0 == Leaf Node가 1개밖에 없는 것
-
-# 하 개어렵다 진짜 너무 헷갈려 ㅠㅠ
