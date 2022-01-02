dacfe913a70dd442b41cceb31e440f42b7eef53f
Hitbee-dev <ckim.esw@gmail.com>
2022-01-02 21:43:36 +0900

    modify

--- programmus/solution11.py
+++
@@ -1,9 +0,0 @@
-arr = list(
-    map(
-        lambda x: list(
-            map(
-                lambda y: int(y), x.split(','))
-        ), 
-        s.replace('},', '|').replace('{', '').replace('}', '').split('|')
-    )
-)
