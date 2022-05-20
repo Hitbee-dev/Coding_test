e834702e7840860e43f18b3b0436b5edb6695a8b
Hitbee-dev <ckim.esw@gmail.com>
2022-05-20 22:34:30 +0900

    solution2.py 이건 또 왜 오답인데

--- swexpert/solution2.py
+++ swexpert/solution2.py
@@ -1,27 +1,14 @@
 T = int(input())
 
 for i in range(1, T+1):
+    sum = 0
     cnt = int(input())
     data = list(map(int, input().split(" ")))
-    buf = []
-    result = []
-    for idx, j in enumerate(data):
-        if buf == []:
-            buf.append(j)
-        elif j >= buf[-1]:
-            buf.append(j)
-        elif j < buf[-1]:
-            result.append(buf)
-            buf = []
-            buf.append(j)
-        if idx == len(data)-1:
-            result.append(buf)
-    sum = 0
-    for index in result:
-        for answer in range(len(index)):
-            if len(index) == 1 or min(index) == max(index):
-                pass
-            else:
-                sum += max(index)-index[answer]
-    print(f"#{i} {sum}")
+    price = 0
+    for j in range(cnt, 1, -1):
+        if price < data[j-1]:
+            price = data[j-1]
+        if data[j-1] >= data[j-2]:
+            sum += price - data[j-2]
 
+    print(f"#{i} {sum}")
