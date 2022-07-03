b6914eb51448d3a66640ea962e3534e17202efdb
Hitbee-dev <ckim.esw@gmail.com>
2022-07-03 20:19:19 +0900

    2477.py ~ing

--- baekjoon/8.basic_math1/2477.py
+++ baekjoon/8.basic_math1/2477.py
@@ -1,24 +1,20 @@
 recycle = int(input()) # 반복 횟수
-direction = []
-result = []
-flag = 0 # 정방향: 0, 역방향: 1
+dicts = {}
+min_length = []
 
 for i in range(6):
     x, y = map(int, input().split())
-    direction.append(x)
-    result.append(y)
+    if x not in dicts.keys():
+        dicts[x] = y
+    else:
+        min_length.append(y)
+        dicts[x] = dicts[x], y
 
-max_index = result.index(max(result))
-before_index = max_index - 1
-if max_index == 5:
-    after_index = max_index - 6
-else:
-    after_index = max_index + 1
-
-if result[before_index] < result[after_index]: # 만약 +1 인덱스가 더 크다면
-    flag = 1
-max_index -= 6
-if flag == 0:
-    print(((result[max_index] * result[max_index-1]) - (result[max_index+2] * result[max_index+3]))* recycle)
-else:
-    print(((result[max_index] * result[max_index+1]) + (result[max_index+3] * result[max_index+4]))*recycle)
+max_length = []
+buf = []
+for i in dicts.values(): # 제일 큰 넓이 구하기
+    if type(i) != tuple:
+        max_length.append(i)
+    else:
+        buf.extend(i)
+print(((max_length[0] * max_length[1]) - (min_length[0] * (buf[buf.index(min_length[0])-3])))*recycle)
